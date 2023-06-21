839b0d638f0f3144f22fd9aeb49845d7d8741a5f
Vladyslav Blazhenko <78543274+VBlazhenko@users.noreply.github.com>
2023-06-21 16:18:07 +0100

    Add files via upload
    lesson 2 updated

---
+++ Lesson 2/index.js
@@ -0,0 +1,24 @@
+// Booleans
+
+let positive = true;
+let negative = false;
+
+// Conditonals
+
+let happy = true;
+
+if (happy) { 
+    console.log("I am happy")
+}
+
+// another example
+
+let isHappy = true;
+
+if (isHappy) {
+  console.log("I am happy 😄");
+} else {
+  console.log("I am not happy 😢");
+}
+
+// logical operators 
